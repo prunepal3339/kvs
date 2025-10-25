4a22b1d376de2148abff2f482e582f5115ffb117
Purshottam Nepal <prunepal3339@gmail.com>
2025-10-25 07:27:31 +0545

    Modify map data structure with RWMutex for concurrent read/write.

--- handler/handler.go
+++ handler/handler.go
@@ -1,6 +1,8 @@
 package handler
 
 import (
+	"sync"
+
 	"github.com/prunepal3339/kvs/resp"
 )
 
@@ -14,8 +16,17 @@ var Handlers = map[string]func([]resp.Value) resp.Value{
 	"HGETALL": hgetall,
 }
 
-var mySet = make(map[string]any)
-var myHSet = make(map[string]map[string]any)
+type SafeMap struct {
+	sync.RWMutex
+	data map[string]any
+}
+type SafeHMap struct {
+	sync.RWMutex
+	data map[string]map[string]any
+}
+
+var mySet = &SafeMap{data: make(map[string]any)}
+var myHSet = &SafeHMap{data: make(map[string]map[string]any)}
 
 func ping(args []resp.Value) resp.Value {
 	if len(args) == 0 {
@@ -32,7 +43,9 @@ func get(args []resp.Value) resp.Value {
 		return resp.NewValue(resp.TAG_ERR, "Expected exactly 1 argument for GET command")
 	}
 	key := args[0].Val().(string)
-	value, ok := mySet[key]
+	mySet.RLock()
+	value, ok := mySet.data[key]
+	mySet.RUnlock()
 	if !ok {
 		return resp.NewValue(resp.TAG_NIL, nil)
 	}
@@ -44,7 +57,9 @@ func set(args []resp.Value) resp.Value {
 	}
 	key := args[0].Val().(string)
 	value := args[1].Val()
-	mySet[key] = value
+	mySet.Lock()
+	mySet.data[key] = value
+	mySet.Unlock()
 	return resp.NewValue(resp.TAG_STR, "OK")
 }
 
@@ -54,7 +69,9 @@ func hget(args []resp.Value) resp.Value {
 	}
 	hash := args[0].Val().(string)
 	key := args[1].Val().(string)
-	value, ok := myHSet[hash][key]
+	myHSet.RLock()
+	value, ok := myHSet.data[hash][key]
+	myHSet.RUnlock()
 	if !ok {
 		return resp.NewValue(resp.TAG_NIL, nil)
 	}
@@ -67,11 +84,12 @@ func hset(args []resp.Value) resp.Value {
 	hash := args[0].Val().(string)
 	key := args[1].Val().(string)
 	value := args[2].Val()
-	if myHSet[hash] == nil {
-		myHSet[hash] = make(map[string]any)
+	myHSet.Lock()
+	if myHSet.data[hash] == nil {
+		myHSet.data[hash] = make(map[string]any)
 	}
-	myHSet[hash][key] = value
-
+	myHSet.data[hash][key] = value
+	myHSet.Unlock()
 	return resp.NewValue(resp.TAG_STR, "OK")
 }
 func hgetall(args []resp.Value) resp.Value {
@@ -79,7 +97,10 @@ func hgetall(args []resp.Value) resp.Value {
 		return resp.NewValue(resp.TAG_ERR, "Expected exactly 1 argument for HGETALL command")
 	}
 	hash := args[0].Val().(string)
-	value := myHSet[hash]
+	myHSet.RLock()
+	value := myHSet.data[hash]
+	myHSet.RUnlock()
+
 	var arrayValue []resp.Value
 	for k, v := range value {
 		arrayValue = append(arrayValue, resp.NewValue(resp.TAG_STR, k))
